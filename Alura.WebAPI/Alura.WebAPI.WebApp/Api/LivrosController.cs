87d164131d21c2203b5d45b00b09b88ec9481f8c
Jonathan Raphael <jonathanraphael74@outlook.com>
2020-12-26 20:15:04 -0300

    added ListasLeituraController

--- Alura.WebAPI/Alura.WebAPI.WebApp/Api/LivrosController.cs
+++ Alura.WebAPI/Alura.WebAPI.WebApp/Api/LivrosController.cs
@@ -1,10 +1,13 @@
+using System.Linq;
 using Alura.ListaLeitura.Modelos;
 using Alura.ListaLeitura.Persistencia;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Alura.WebAPI.WebApp.Api
 {
-    public class LivrosController : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class LivrosController : ControllerBase
     {
         private readonly IRepository<Livro> _repo;
 
@@ -14,6 +17,13 @@ namespace Alura.WebAPI.WebApp.Api
         }
 
         [HttpGet]
+        public IActionResult ListaDeLivros()
+        {
+            var livros = _repo.All.Select(l => l.ToApi()).ToList();
+            return Ok(livros);
+        }
+
+        [HttpGet("{id}")]
         public IActionResult Recuperar(int id)
         {
             var model = _repo.Find(id);
@@ -23,7 +33,54 @@ namespace Alura.WebAPI.WebApp.Api
                 return NotFound();
             }
 
-            return Json(model.ToModel());
+            return Ok(model.ToApi());
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Incluir([FromBody] LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                _repo.Incluir(livro);
+
+                var uri = Url.Action("Recuperar", new { id = livro.Id });
+
+                return Created(uri, livro);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut]
+        public IActionResult Alterar([FromBody] LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                if (model.Capa == null)
+                {
+                    livro.ImagemCapa = _repo.All
+                        .Where(l => l.Id == livro.Id)
+                        .Select(l => l.ImagemCapa)
+                        .FirstOrDefault();
+                }
+                _repo.Alterar(livro);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remover(int id)
+        {
+            var model = _repo.Find(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            _repo.Excluir(model);
+            return NoContent();
         }
     }
 }
