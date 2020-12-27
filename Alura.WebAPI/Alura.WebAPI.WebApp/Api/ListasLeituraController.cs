303757c49a07b709851e881c69a394d5f5beeeb8
Jonathan Raphael <jonathanraphael74@outlook.com>
2020-12-27 17:42:04 -0300

    added authentication with JWT

--- Alura.WebAPI/Alura.WebAPI.WebApp/Api/ListasLeituraController.cs
+++ Alura.WebAPI/Alura.WebAPI.WebApp/Api/ListasLeituraController.cs
@@ -2,11 +2,13 @@ using System.Collections.Generic;
 using System.Linq;
 using Alura.ListaLeitura.Modelos;
 using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Lista = Alura.ListaLeitura.Modelos.ListaLeitura;
 
 namespace Alura.WebAPI.WebApp.Api
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class ListaLeituraController : ControllerBase
--- Alura.WebAPI/Alura.WebAPI.WebApp/Api/LivrosController.cs
+++ Alura.WebAPI/Alura.WebAPI.WebApp/Api/LivrosController.cs
@@ -1,12 +1,14 @@
 using System.Linq;
 using Alura.ListaLeitura.Modelos;
 using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Alura.WebAPI.WebApp.Api
 {
+    [Authorize]
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class LivrosController : ControllerBase
     {
         private readonly IRepository<Livro> _repo;
